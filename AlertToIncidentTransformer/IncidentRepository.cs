911a24dc9feb0646e650319c57d6f1069e7ed38c
Patrick Gawley <patrick.gawley@outlook.com>
2021-05-28 17:09:05 +0100

    Updated Incident repository with lease locking

--- AlertToIncidentTransformer/IncidentRepository.cs
+++ AlertToIncidentTransformer/IncidentRepository.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Specialized;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace AlertToIncidentTransformer
 {
     public interface IIncidentRepository
     {
-        List<Incident> GetIncidents();
+        Task<List<Incident>> GetIncidents();
         bool AcquireLock();
         bool IsLocked();
         bool PostIncidents(List<Incident> incidents);
@@ -16,30 +21,51 @@ namespace AlertToIncidentTransformer
     public class IncidentRepository : IIncidentRepository
     {
         private bool _isLocked = false;
-        private readonly string _storageConnectionString, _incidentContainer;
+        private readonly BlobServiceClient _blobServiceClient;
+        private readonly BlobContainerClient _containerClient;
+        private readonly BlobClient _blobClient;
+        private readonly BlobLeaseClient _blobLeaseClient;
+
 
         public IncidentRepository(string storageConnectionString, string incidentContainer = "incidents", string incidentFile = "currentIncidents.json")
         {
-            _storageConnectionString = storageConnectionString;
-            _incidentContainer = incidentContainer;
+            _blobServiceClient = new BlobServiceClient(storageConnectionString);
+            _containerClient = _blobServiceClient.GetBlobContainerClient(incidentContainer);
+            _blobClient = _containerClient.GetBlobClient(incidentFile);
+            _blobLeaseClient = _blobClient.GetBlobLeaseClient();
+
         }
 
         public bool AcquireLock()
         {
+            _blobLeaseClient.Acquire(TimeSpan.FromSeconds(1));
             _isLocked = true;
             return true;
         }
 
         public bool ReleaseLock()
         {
+            _blobLeaseClient.Release();
             _isLocked = false;
             return true;
         }
 
-        public List<Incident> GetIncidents()
+        public async Task<List<Incident>> GetIncidents()
         {
-            return new List<Incident>();
+            var incidents = new List<Incident>();
+
+            if (await _blobClient.ExistsAsync())
+            {
+                
+                var response = await _blobClient.DownloadAsync();
+                
+                incidents = await JsonSerializer.DeserializeAsync<List<Incident>>(response.Value.Content);
+                
+            }
+
+            return incidents;
         }
+
 
         public bool IsLocked() => _isLocked;
 
--- AlertToIncidentTransformerTests/IncidentRepositoryUnitTests.cs
+++ AlertToIncidentTransformerTests/IncidentRepositoryUnitTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Xunit;
 using FluentAssertions;
+using AlertToIncidentTransformer;
 
 namespace AlertToIncidentTransformerTests
 {
@@ -11,6 +12,10 @@ namespace AlertToIncidentTransformerTests
         [Fact]
         public void GivenNoIncidents_WhenGetIncidents_ThenNoIcidentsReturned()
         {
+            var givenNoIncidents = new IncidentRepository("UseDevelopmentStorage=true;", "GivenNoIncidents_WhenGetIncidents_ThenNoIcidentsReturned");
+
+            var whenGetIncidents = givenNoIncidents.GetIncidents().Result;
+
 
         }
     }
